762160e84b255db57cb9b1da0de42640ad10162a
vveeps <54472340+vveeps@users.noreply.github.com>
2021-10-26 17:57:42 +0300

    dont error on imageless spotify tracks

--- pomice/spotify/track.py
+++ pomice/spotify/track.py
@@ -1,11 +1,17 @@
 class Track:
     """The base class for a Spotify Track"""
+
     def __init__(self, data: dict) -> None:
         self.name = data['name']
         self.artists = ", ".join(artist["name"] for artist in data['artists'])
         self.length = data['duration_ms']
         self.id = data['id']
-        self.image = data['album']['images'][0]['url'] if data.get('album') else None
+
+        if data.get("album") and data["album"]["images"]:
+            self.image = data['album']['images'][0]['url']
+        else:
+            self.image = None
+
         self.uri = data['external_urls']['spotify']
 
     def __repr__(self) -> str:
