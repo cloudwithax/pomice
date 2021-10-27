89b18e7a2a537c8afd434bcd7ad794313809e297
cloudwithax <nickisme14@gmail.com>
2021-10-27 17:56:24 -0400

    Changed versioning for PyPI release

--- pomice/__init__.py
+++ pomice/__init__.py
@@ -11,7 +11,7 @@ if discord.__version__ != '2.0.0a':
         "using 'pip install git+https://github.com/Rapptz/discord.py@master'"
     )
 
-__version__ = "1.0.7.1"
+__version__ = "1.0.7.2"
 __title__ = "pomice"
 __author__ = "cloudwithax"
 
