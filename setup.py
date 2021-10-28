8c9e1114dadb55df3c608fec430ee367bd88cf73
vveeps <54472340+vveeps@users.noreply.github.com>
2021-10-28 18:04:45 +0300

    bump version to 1.1.0 because changes are majorish

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 setuptools.setup(
     name="pomice",
     author="cloudwithax",
-    version="1.0.7.2",
+    version="1.1.0",
     url="https://github.com/cloudwithax/pomice",
     packages=setuptools.find_packages(),
     license="GPL",
