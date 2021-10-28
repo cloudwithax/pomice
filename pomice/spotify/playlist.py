ff1c089a754dbc46c1d760db6c8de4cfcc664f01
Crussader <75786691+Crussader@users.noreply.github.com>
2021-10-28 18:25:38 +0400

    spotify code cleanup

--- pomice/spotify/playlist.py
+++ pomice/spotify/playlist.py
@@ -1,16 +1,21 @@
 from .track import Track
 from typing import List
 
+
 class Playlist:
     """The base class for a Spotify playlist"""
+
     def __init__(self, data: dict, tracks: List[Track]) -> None:
-        self.name = data['name']
+        self.name = data["name"]
         self.tracks = tracks
-        self.owner = data['owner']['display_name']
-        self.total_tracks = data['tracks']['total']
-        self.id = data['id']
-        self.image = data['images'][0]['url']
-        self.uri = data['external_urls']['spotify']
+        self.owner = data["owner"]["display_name"]
+        self.total_tracks = data["tracks"]["total"]
+        self.id = data["id"]
+        self.image = data["images"][0]["url"]
+        self.uri = data["external_urls"]["spotify"]
 
     def __repr__(self) -> str:
-        return f"<Pomice.spotify.Playlist name={self.name} owner={self.owner} id={self.id} total_tracks={self.total_tracks} tracks={self.tracks}>"
+        return (
+            f"<Pomice.spotify.Playlist name={self.name} owner={self.owner} id={self.id} "
+            f"total_tracks={self.total_tracks} tracks={self.tracks}>"
+        )
