f7a830dc3a38f74756754c2077946cd72b37a93b
Crussader <75786691+Crussader@users.noreply.github.com>
2021-10-28 18:23:19 +0400

    spotify code cleanup

--- pomice/spotify/__init__.py
+++ pomice/spotify/__init__.py
@@ -1,6 +1,6 @@
 """Spotify module for Pomice, made possible by cloudwithax 2021"""
 
-from .exceptions import SpotifyRequestException
+from .exceptions import InvalidSpotifyURL, SpotifyRequestException
 from .track import Track
 from .playlist import Playlist
 from .album import Album
