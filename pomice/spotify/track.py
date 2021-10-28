d87a6644f6909420ac3984e183823e22bd198b74
Crussader <75786691+Crussader@users.noreply.github.com>
2021-10-28 18:26:03 +0400

    spotify code cleanup

--- pomice/spotify/track.py
+++ pomice/spotify/track.py
@@ -1,18 +1,22 @@
+
 class Track:
     """The base class for a Spotify Track"""
 
     def __init__(self, data: dict) -> None:
-        self.name = data['name']
-        self.artists = ", ".join(artist["name"] for artist in data['artists'])
-        self.length = data['duration_ms']
-        self.id = data['id']
+        self.name = data["name"]
+        self.artists = ", ".join(artist["name"] for artist in data["artists"])
+        self.length = data["duration_ms"]
+        self.id = data["id"]
 
         if data.get("album") and data["album"]["images"]:
-            self.image = data['album']['images'][0]['url']
+            self.image = data["album"]["images"][0]["url"]
         else:
             self.image = None
 
-        self.uri = data['external_urls']['spotify']
+        self.uri = data["external_urls"]["spotify"]
 
     def __repr__(self) -> str:
-        return f"<Pomice.spotify.Track name={self.name} artists={self.artists} length={self.length} id={self.id}>"
+        return (
+            f"<Pomice.spotify.Track name={self.name} artists={self.artists} "
+            f"length={self.length} id={self.id}>"
+        )
