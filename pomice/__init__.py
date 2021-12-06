50429277ea7c865d3896c26d5711e9ad10bbc33c
cloudwithax <nickisme14@gmail.com>
2021-12-06 17:58:21 -0500

    Fix __init__ to support discord.py forks

--- pomice/__init__.py
+++ pomice/__init__.py
@@ -1,14 +1,14 @@
 """Pomice wrapper for Lavalink, made possible by cloudwithax 2021"""
 import discord
 
-if discord.__version__ != "2.0.0a":
+if not discord.__version__.startswith("2.0"):
     class DiscordPyOutdated(Exception):
         pass
 
     raise DiscordPyOutdated(
-        "You must have discord.py 2.0 to use this library. "
-        "Uninstall your current version and install discord.py 2.0 "
-        "using 'pip install git+https://github.com/Rapptz/discord.py@master'"
+        "You must have discord.py 2.0 or a discord.py fork that uses the 'discord' namespace "
+        "(a.k.a: you import the libary using 'import discord') to use this library. "
+        "Uninstall your current version and install discord.py 2.0 or a compatible fork."
     )
 
 __version__ = "1.1.6"
