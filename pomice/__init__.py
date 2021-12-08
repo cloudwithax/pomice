0a78a8f5c79b144db0f53a2d25685c30992e832d
RPS <vincentbusiness55@gmail.com>
2021-12-08 02:14:53 -0800

    Make ``__init__`` better

--- pomice/__init__.py
+++ pomice/__init__.py
@@ -1,4 +1,11 @@
-"""Pomice wrapper for Lavalink, made possible by cloudwithax 2021"""
+"""
+Pomice
+~~~~~~
+Pythonic Wrapper for Lavalink
+
+:copyright: 2021 cloudwithax
+:license: GPL-3.0
+"""
 import discord
 
 if not discord.__version__.startswith("2.0"):
