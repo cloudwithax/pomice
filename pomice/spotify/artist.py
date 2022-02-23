13d0df8f97a6d4bac868c9bdfdbb2c5d9a8559d5
MiroslavRosenov <57327793+MiroslavRosenov@users.noreply.github.com>
2022-02-23 11:06:15 +0200

    Update artist.py

--- pomice/spotify/artist.py
+++ pomice/spotify/artist.py
@@ -1,1 +1,18 @@
+class Artist:
+    """The base class for a Spotify artist"""
 
+    def __init__(self, data: dict, tracks: dict) -> None:
+        self.name = f"Top tracks for {data['name']}" 
+        self.genres = ", ".join(genre for genre in data["genres"])
+        self.followers = data["followers"]["total"]
+        self.image = data["images"][0]["url"]
+        self.tracks = tracks
+        self.total_tracks = len(tracks)
+        self.id = data["id"]
+        self.uri = data["external_urls"]["spotify"]
+
+    def __repr__(self) -> str:
+        return (
+            f"<Pomice.spotify.Artist name={self.name} id={self.id} "
+            f"total_tracks={self.total_tracks} tracks={self.tracks}>"
+        )
