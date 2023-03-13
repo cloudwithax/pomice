dde6e3711c61998adbe23d9005bf152283fd3d43
cloudwithax <heyimnick14@gmail.com>
2023-03-12 23:00:23 -0400

    2.3

--- pomice/__init__.py
+++ pomice/__init__.py
@@ -20,7 +20,7 @@ if not discord.version_info.major >= 2:
         "using 'pip install discord.py'",
     )
 
-__version__ = "2.3a"
+__version__ = "2.3"
 __title__ = "pomice"
 __author__ = "cloudwithax"
 __license__ = "GPL-3.0"
