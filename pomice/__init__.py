f6fffcc149c2dea2ee26714bba7897540a333bc8
Zander M <zander@zandercraft.ca>
2023-05-03 17:42:04 -0400

    Version bump (2.5.1)

--- pomice/__init__.py
+++ pomice/__init__.py
@@ -20,7 +20,7 @@ if not discord.version_info.major >= 2:
         "using 'pip install discord.py'",
     )
 
-__version__ = "2.5.0"
+__version__ = "2.5.1"
 __title__ = "pomice"
 __author__ = "cloudwithax"
 __license__ = "GPL-3.0"
