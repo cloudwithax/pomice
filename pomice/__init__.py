0dccb9b496c61b71868ce10a628c5ad8afca1ac4
cloudwithax <heyimnick14@gmail.com>
2023-05-23 21:37:01 -0400

    we are no longer a wrapper for discord.py :)

--- pomice/__init__.py
+++ pomice/__init__.py
@@ -1,26 +1,14 @@
 """
 Pomice
 ~~~~~~
-The modern Lavalink wrapper designed for discord.py.
+The modern Lavalink wrapper designed for Python.
 
 Copyright (c) 2023, cloudwithax
 
 Licensed under GPL-3.0
 """
-import discord
 
-if not discord.version_info.major >= 2:
-
-    class DiscordPyOutdated(Exception):
-        pass
-
-    raise DiscordPyOutdated(
-        "You must have discord.py (v2.0 or greater) to use this library. "
-        "Uninstall your current version and install discord.py 2.0 "
-        "using 'pip install discord.py'",
-    )
-
-__version__ = "2.7.0"
+__version__ = "3.0.0a"
 __title__ = "pomice"
 __author__ = "cloudwithax"
 __license__ = "GPL-3.0"
